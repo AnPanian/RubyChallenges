5c8f464ef8d05c61aabc3665c998fa8b69248be9
amber_p <panianamber@gmail.com>
2020-06-30 07:43:34 -0600

    Ruby Practice Challenges

---
+++ always_three.rb
@@ -0,0 +1,13 @@
+puts "give me a number" 
+
+number1 = gets.to_i
+
+mid_number = number1 + 5
+mid_number = mid_number * 2
+mid_number = mid_number - 4
+mid_number = mid_number / 2
+
+final_number = mid_number - number1
+
+puts "Always #{final_number}"
+
